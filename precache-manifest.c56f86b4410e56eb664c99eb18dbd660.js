a41dd541b7fb19f05fd64a4dfce9656012675c88
Maarten van der Heijden <maarten.van.der.heijden@ing.com>
2022-11-27 18:25:26 +0100

    Updates

---
+++ precache-manifest.c56f86b4410e56eb664c99eb18dbd660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50186b01f0843b6eec49",
+    "url": "/static/css/main.b49173ec.chunk.css"
+  },
+  {
+    "revision": "50186b01f0843b6eec49",
+    "url": "/static/js/main.4e7b87a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6d53937671abfe74d8d",
+    "url": "/static/css/2.9b7aaded.chunk.css"
+  },
+  {
+    "revision": "d6d53937671abfe74d8d",
+    "url": "/static/js/2.50deb6c2.chunk.js"
+  },
+  {
+    "revision": "ff6bd5ec5d6e362c29a50ed467237c8b",
+    "url": "/index.html"
+  }
+];
